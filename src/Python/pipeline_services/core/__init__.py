29d789a1d23e9838b774f869225d2d915d950c0f
Nektarios Kalogridis <mail@nektarios.com>
2026-08-04 19:57:49 -0400

    refactor(python): export vNext core surface

--- src/Python/pipeline_services/core/__init__.py
+++ src/Python/pipeline_services/core/__init__.py
@@ -1,5 +1,24 @@
-from .pipeline import ActionControl, ActionTiming, Pipeline, PipelineError, PipelineResult, StepControl
-from .pipeline_provider import PipelineProvider, PipelineProviderMode
+from .pipeline import (
+    Action,
+    ActionControl,
+    ActionTiming,
+    InvalidErrorHandlerError,
+    Pipeline,
+    PipelineError,
+    PipelineObserver,
+    PipelineResult,
+    StepAction,
+    StepControl,
+    UnaryOperator,
+    short_circuit,
+)
+from .pipeline_provider import (
+    PipelineProvider,
+    PipelineProviderMode,
+    default_instance_count,
+    default_pool_max,
+)
+from .pipeline_router import PipelineRouter
 from .registry import PipelineRegistry
 from .runtime_pipeline import RuntimePipeline
 from .metrics_actions import print_metrics
